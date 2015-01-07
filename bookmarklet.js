05d950252a8f4b77bd36a5cfc2c3b58d52811068
thirdy <thirdy.derivera@gmail.com>
2015-01-07 20:14:12 +0800

    Update bookmarklet.js
    bookmarklet is now hosted on google drive. and it's working with Chrome.

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,8 +1,8 @@
 
-javascript:%20(function%20()%20{%20%20%20%20%20%20var%20jsCode%20=%20document.createElement('script');%20%20%20%20%20%20jsCode.setAttribute('src',%20'https://raw.githubusercontent.com/thirdy/forgotten-mods/master/forgotten-mods.js');%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20document.body.appendChild(jsCode);%20%20%20}());
+javascript:%20(function%20()%20{%20var%20jsCode%20=%20document.createElement('script');%20jsCode.setAttribute('src',%20'https://76f193c373fb5126bd1b5ee4fc270e3c6a1833cb.googledrive.com/host/0B4u8zcQBGBwpRll0ZzJxZ09DUUU/forgotten-mods.js');%20%20document.body.appendChild(jsCode);%20}());
 
 // Below is the version where it should be better, WIP
-$.get( "test.php", function( data ) {
-alert( "Data Loaded: " + data );
-});
-eval(  'alert("h")' )
+//$.get( "test.php", function( data ) {
+//alert( "Data Loaded: " + data );
+//});
+//eval(  'alert("h")' )
