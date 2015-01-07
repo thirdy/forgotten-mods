421ce4cd2dde4e21f5f5559c8cb38454afc6963c
thirdy <thirdy.derivera@gmail.com>
2015-01-07 12:26:24 +0800

    Create bookmarklet.js

---
+++ bookmarklet.js
@@ -0,0 +1,8 @@
+
+javascript:%20(function%20()%20{%20%20%20%20%20%20var%20jsCode%20=%20document.createElement('script');%20%20%20%20%20%20jsCode.setAttribute('src',%20'https://raw.githubusercontent.com/thirdy/forgotten-mods/master/forgotten-mods.js');%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20document.body.appendChild(jsCode);%20%20%20}());
+
+// Below is the version where it should be better, WIP
+$.get( "test.php", function( data ) {
+alert( "Data Loaded: " + data );
+});
+eval(  'alert("h")' )
