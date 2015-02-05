a29f65e81d1421ec20733a9df18cf21a874e5101
thirdy <thirdy.derivera@gmail.com>
2015-02-05 08:28:12 +0800

    added support for options

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,8 +1,18 @@
 
-javascript:%20(function%20()%20{%20var%20jsCode%20=%20document.createElement('script');%20jsCode.setAttribute('src',%20'https://76f193c373fb5126bd1b5ee4fc270e3c6a1833cb.googledrive.com/host/0B4u8zcQBGBwpRll0ZzJxZ09DUUU/forgotten-mods.js');%20%20document.body.appendChild(jsCode);%20}());
+javascript:(function(){
+var chrome_options_fm_items = {'wtbpm_template':'@{ign} Hi, WTB your {item} for {price} in {league}. You can invite me to party whenever you are free. Thanks!',
+  	 'wtbpmwoprice_template':'@{ign} Hi, WTB your {item} in {league}. You can invite me to party whenever you are free. Thanks!',
+	 'item_notes_enabled':true,
+	 'seller_notes_enabled':true};
 
-// Below is the version where it should be better, WIP
-//$.get( "test.php", function( data ) {
-//alert( "Data Loaded: " + data );
-//});
-//eval(  'alert("h")' )
+var fm_options_elem = document.createElement('fm_options_elem');
+for(i in chrome_options_fm_items) {
+  fm_options_elem.setAttribute(i, chrome_options_fm_items[i]);
+}
+
+var jsCode = document.createElement('script'); jsCode.setAttribute('src', 'https://76f193c373fb5126bd1b5ee4fc270e3c6a1833cb.googledrive.com/host/0B4u8zcQBGBwpRll0ZzJxZ09DUUU/forgotten-mods.js');  document.body.appendChild(jsCode); document.body.appendChild(fm_options_elem);
+})()
+
+
+// bookmarket
+javascript:(function(){%20var%20chrome_options_fm_items%20=%20{'wtbpm_template':'@{ign}%20Hi,%20WTB%20your%20{item}%20for%20{price}%20in%20{league}.%20You%20can%20invite%20me%20to%20party%20whenever%20you%20are%20free.%20Thanks!',%20%20%20%20'wtbpmwoprice_template':'@{ign}%20Hi,%20WTB%20your%20{item}%20in%20{league}.%20You%20can%20invite%20me%20to%20party%20whenever%20you%20are%20free.%20Thanks!',%20%20'item_notes_enabled':true,%20%20'seller_notes_enabled':true};%20%20var%20fm_options_elem%20=%20document.createElement('fm_options_elem');%20for(i%20in%20chrome_options_fm_items)%20{%20%20%20fm_options_elem.setAttribute(i,%20chrome_options_fm_items[i]);%20}%20%20var%20jsCode%20=%20document.createElement('script');%20jsCode.setAttribute('src',%20'https://76f193c373fb5126bd1b5ee4fc270e3c6a1833cb.googledrive.com/host/0B4u8zcQBGBwpRll0ZzJxZ09DUUU/forgotten-mods.js');%20%20document.body.appendChild(jsCode);%20document.body.appendChild(fm_options_elem);%20})()
